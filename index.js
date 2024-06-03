2aa55194dfcaf6a366839f6d999ae87b77843e74
sidchopp <sidchopp@gmail.com>
2024-06-03 12:19:21 -0600

    get documents, get a single document methods implemented

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 import express from "express";
+import { ObjectId } from "mongodb";
+
 import { getDb, connectToDb } from "./db.js";
 
 //init app and middleware
 const app = express();
 
 // routes
-app.get("/", (req, res) => {
-  res.json("Welcome to Node MongoDB API!");
+
+// GET all books
+app.get("/books", async (req, res) => {
+  let books = [];
+
+  try {
+    await db
+      .collection("books")
+      .find()
+      .sort({ author: 1 })
+      .forEach((book) => books.push(book));
+
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ error: "Could not fetch the documents" });
+  }
+});
+
+// GET a Book
+app.get("/books/:id", async (req, res) => {
+  try {
+    if (ObjectId.isValid(req.params.id)) {
+      const doc = await db
+        .collection("books")
+        .findOne({ _id: new ObjectId(req.params.id) });
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ error: "Document not found" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid ID format" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Could not fetch the document" });
+  }
 });
 
 //db connection
@@ -19,8 +56,5 @@ connectToDb((err) => {
       console.log("Server is listening on port 4000..");
     });
     db = getDb();
-
-    //console log to test db object that's received
-    console.log(db);
   }
 });
